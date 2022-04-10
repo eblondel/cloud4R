30460d1603615c9de241b2b691e49d34373d847d
eblondel <emmanuel.blondel1@gmail.com>
2022-04-10 13:57:13 +0200

    more tests

--- tests/testthat/test_import.R
+++ tests/testthat/test_import.R
@@ -11,6 +11,10 @@ test_that("atom4R works",{
 	expect_true(length(classes)>0)
 	clazz <- atom4R::DCElement$getClassByElement("creator")
 	expect_is(clazz, "R6ClassGenerator")
+	dcentry <- atom4R::DCEntry$new()
+	dcentry$addDCCreator("someone")
+	xml <- dcentry$encode()
+	expect_is(xml, "XMLInternalNode")
 })
 
 test_that("zen4R works",{
